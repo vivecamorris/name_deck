8508ca920ec13268f4aa8b73abcacc2a3dd6a78f
vivecamorris <viveca.morris@yale.edu>
2014-06-17 14:30:22 -0400

    init

--- namedeck.rb
+++ namedeck.rb
@@ -11,15 +11,36 @@ get '/' do
 	erb :index
 end
 
-get '/new' do
-	erb :new
+get '/results' do
+	erb :results
 end
 
-post '/new' do
-	newdeck = Deck.new(params[:setname], [], params[:setdescription])
-	newdeck.add_person(params[:name], params[:photourl], params[:description])
-	session['alldecks'] << newdeck
+get '/newdeck' do
+	erb :newdeck
+end
+
+post '/newdeck' do
+	params[:deckname] = Deck.new(params[:deckname], [], params[:deckdescription])
+	# newdeck.add_person(params[:name], params[:photourl], params[:description])
+	session['alldecks'] << params[:deckname]
 	erb :results, :locals => {:alldecks => session['alldecks']}
+	redirect '/results'
+end
+
+get '/:deckname/newperson' do
+	erb :newperson
+end
+
+get '/newperson' do
+	erb :newperson
+end
+
+post '/:deckname/newperson' do
+	deckname = params[:deckname]
+	newperson = Person.new(params[:name],params[:photourl],params[:description])
+	deckname.add_person(newperson)
+	erb :results, :locals => {:alldecks => session['alldecks']}
+	redirect '/results'
 end
 
 class Person
@@ -34,20 +55,20 @@ class Person
 end
 
 class Deck
-	def initialize (setname,setpeople=[],setdescription)
-		@setname = setname.to_s
-		@setpeople = setpeople
-		@setdescription = setdescription.to_s
+	def initialize (deckname,deckpeople=[],deckdescription)
+		@deckname = deckname.to_s
+		@deckpeople = deckpeople
+		@deckdescription = deckdescription.to_s
 	end
 
-	attr_accessor :setname, :setdescription, :setpeople
+	attr_accessor :deckname, :deckdescription, :deckpeople
 
-	def add_person (name,url,description=nil)
-		@setpeople.push(Person.new(name,url,description))
+	def add_person (person)
+		@deckpeople.push(person)
 	end
 
 	def delete_person (name)
-		@setpeople.delete_if {|n| n.name == name}
+		@deckpeople.delete_if {|n| n.name == name}
 	end
 
 end
