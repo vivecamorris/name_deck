2699e7f7070cbcb0a825c267cfebe4a4d996dc88
vivecamorris <viveca.morris@yale.edu>
2014-06-18 14:59:50 -0400

    randomly generating photos with input box

--- decks.rb
+++ decks.rb
@@ -1,11 +1,12 @@
 class Person
-	def initialize (name,url,description=nil)
+	def initialize (name,url,description=nil,quizzed=false)
 		@name = name.to_s
 		@url = url.to_s
 		@description = description.to_s
+		@quizzed = quizzed
 	end
 
-	attr_accessor :name, :url, :description
+	attr_accessor :name, :url, :description, :quizzed
 
 end
 
@@ -18,8 +19,8 @@ class Deck
 
 	attr_accessor :deckname, :deckdescription, :deckpeople
 
-	def add_person (name, photo, description)
-		person = Person.new(name, photo, description)
+	def add_person (name, photo, description, quizzed)
+		person = Person.new(name, photo, description, quizzed)
 		@deckpeople.push(person)
 	end
 
@@ -32,37 +33,37 @@ end
 class BootcampData
 	def initialize
 		@bootcamp = Deck.new("Tech Bootcamp 2014", [], "Code Monkeys")
-		@bootcamp.add_person("Viveca Morris", "photos/viveca_morris.jpg", "Stiles 2015")
-		@bootcamp.add_person("Tim Follo", "photos/tim_follo.jpg", "Morse 2016")
-		@bootcamp.add_person("Paul Styslinger", "photos/paul_styslinger.jpg", "Stiles 2017")
-		@bootcamp.add_person("Patrick Casey", "photos/patrick_casey.jpg", "Saybrook 2015")
-		@bootcamp.add_person("Andres Vargas", "photos/andres_vargas.jpg", "Calhoun 2017")
-		@bootcamp.add_person("E-lynn Yap", "photos/elynn_yap.jpg", "Timothy Dwight 2014")
-		@bootcamp.add_person("Taylor Stibs", "photos/taylor_stibs.jpg", "Branford 2015")
-		@bootcamp.add_person("Natalia Castillejo Arias", "photos/natalia_castillejo.jpg", "JE 2016")
-		@bootcamp.add_person("David Liu", "photos/david_liu.jpg", "Berkeley 2017")
-		@bootcamp.add_person("Nikki Feldman", "photos/nikki_feldman.jpg", "Davenport 2014")
-		@bootcamp.add_person("Loide Marwanga", "photos/loide_marwanga.jpg", "School of Art 2015")
-		@bootcamp.add_person("Jay Hou", "photos/jay_hou.jpg", "Trumbull 2017")
-		@bootcamp.add_person("Sonja Peterson", "photos/sonja_peterson.jpg", "Davenport 2014")
-		@bootcamp.add_person("Caroline Posner", "photos/caroline_posner.jpg", "Berkeley 2017")
-		@bootcamp.add_person("Aily Zhang", "photos/aily_zhang.jpg", "Silliman 2015")
-		@bootcamp.add_person("Aileen Huang", "photos/aileen_huang.jpg", "Berkeley 2017")
-		@bootcamp.add_person("Rachel Prince", "photos/rachel_prince.jpg", "Davenport 2017")
-		@bootcamp.add_person("Phil Esterman", "photos/phil_esterman.jpg", "Branford 2017")
-		@bootcamp.add_person("Ben Burke", "photos/ben_burke.jpg", "Pierson 2015")
-		@bootcamp.add_person("Amy Rockwood", "photos/amy_rockwood.jpg", "Berkeley 2016")
-		@bootcamp.add_person("Natalia Bueno", "photos/natalia_bueno.jpg", "GSAS 2017")
-		@bootcamp.add_person("Joe Calamia", "photos/Joseph_Cala.jpg", "Forestry 2015")
-		@bootcamp.add_person("Zach Young", "photos/zach_young.jpg", "Silliman 2017")
-		@bootcamp.add_person("David McPeek", "photos/david_mcpeek.jpg", "Berkeley 2016")
-		@bootcamp.add_person("Sahil Gupta", "photos/sahil_gupta.jpg", "Saybrook 2017")
-		@bootcamp.add_person("Henok Addis", "photos/henok_addis.jpg", "Stiles 2017")
-		@bootcamp.add_person("Jason Kim", "photos/jason_kim.jpg", "Silliman 2016")
-		@bootcamp.add_person("Charlie Proctor", "photos/charlie_proctor.jpg", "TD 2017")
-		@bootcamp.add_person("Marcus Russi", "photos/marcus_russi.jpg", "Branford 2017")
-		@bootcamp.add_person("Casey Watts", "photos/casey_watts.jpg", "Yale ITS")
-		@bootcamp.add_person("Sara Hamilton", "photos/sara_hamilton.jpg", "Stiles 2@016")
+		@bootcamp.add_person("Viveca Morris", "photos/viveca_morris.jpg", "Stiles 2015", false)
+		@bootcamp.add_person("Tim Follo", "photos/tim_follo.jpg", "Morse 2016", false)
+		@bootcamp.add_person("Paul Styslinger", "photos/paul_styslinger.jpg", "Stiles 2017", false)
+		@bootcamp.add_person("Patrick Casey", "photos/patrick_casey.jpg", "Saybrook 2015", false)
+		@bootcamp.add_person("Andres Vargas", "photos/andres_vargas.jpg", "Calhoun 2017", false)
+		@bootcamp.add_person("E-lynn Yap", "photos/elynn_yap.jpg", "Timothy Dwight 2014", false)
+		@bootcamp.add_person("Taylor Stibs", "photos/taylor_stibs.jpg", "Branford 2015", false)
+		@bootcamp.add_person("Natalia Castillejo Arias", "photos/natalia_castillejo.jpg", "JE 2016", false)
+		@bootcamp.add_person("David Liu", "photos/david_liu.jpg", "Berkeley 2017", false)
+		@bootcamp.add_person("Nikki Feldman", "photos/nikki_feldman.jpg", "Davenport 2014", false)
+		@bootcamp.add_person("Loide Marwanga", "photos/loide_marwanga.jpg", "School of Art 2015", false)
+		@bootcamp.add_person("Jay Hou", "photos/jay_hou.jpg", "Trumbull 2017", false)
+		@bootcamp.add_person("Sonja Peterson", "photos/sonja_peterson.jpg", "Davenport 2014", false)
+		@bootcamp.add_person("Caroline Posner", "photos/caroline_posner.jpg", "Berkeley 2017", false)
+		@bootcamp.add_person("Aily Zhang", "photos/aily_zhang.jpg", "Silliman 2015", false)
+		@bootcamp.add_person("Aileen Huang", "photos/aileen_huang.jpg", "Berkeley 2017", false)
+		@bootcamp.add_person("Rachel Prince", "photos/rachel_prince.jpg", "Davenport 2017", false)
+		@bootcamp.add_person("Phil Esterman", "photos/phil_esterman.jpg", "Branford 2017", false)
+		@bootcamp.add_person("Ben Burke", "photos/ben_burke.jpg", "Pierson 2015", false)
+		@bootcamp.add_person("Amy Rockwood", "photos/amy_rockwood.jpg", "Berkeley 2016", false)
+		@bootcamp.add_person("Natalia Bueno", "photos/natalia_bueno.jpg", "GSAS 2017", false)
+		@bootcamp.add_person("Joe Calamia", "photos/Joseph_Cala.jpg", "Forestry 2015", false)
+		@bootcamp.add_person("Zach Young", "photos/zach_young.jpg", "Silliman 2017", false)
+		@bootcamp.add_person("David McPeek", "photos/david_mcpeek.jpg", "Berkeley 2016", false)
+		@bootcamp.add_person("Sahil Gupta", "photos/sahil_gupta.jpg", "Saybrook 2017", false)
+		@bootcamp.add_person("Henok Addis", "photos/henok_addis.jpg", "Stiles 2017", false)
+		@bootcamp.add_person("Jason Kim", "photos/jason_kim.jpg", "Silliman 2016", false)
+		@bootcamp.add_person("Charlie Proctor", "photos/charlie_proctor.jpg", "TD 2017", false)
+		@bootcamp.add_person("Marcus Russi", "photos/marcus_russi.jpg", "Branford 2017", false)
+		@bootcamp.add_person("Casey Watts", "photos/casey_watts.jpg", "Yale ITS", false)
+		@bootcamp.add_person("Sara Hamilton", "photos/sara_hamilton.jpg", "Stiles 2016", false)
 	end
 	attr_reader :bootcamp
 end
--- namedeck.rb
+++ namedeck.rb
@@ -10,9 +10,7 @@ session = {}
 session['alldecks'] = []
 
 get '/' do
-	bootcampdataobj = BootcampData.new
-	puts bootcampdataobj.bootcamp
-	erb :index, :locals => { :bootcamp => bootcampdataobj.bootcamp }
+	erb :index
 end
 
 get '/results' do
@@ -23,16 +21,49 @@ get '/newdeck' do
 	erb :newdeck
 end
 
+post '/test' do
+	if params[:nameinput] == params[:correctname]
+		erb :correct
+	else
+		erb :incorrect
+	end
+end
+
+def get_random_photo(deckpeople)
+	successful = false
+	while successful == false
+		num = rand(deckpeople.length)
+		persontobequizzed = deckpeople[num]
+			if persontobequizzed.quizzed == false
+				persontobequizzed.quizzed = true
+				successful = true
+				return persontobequizzed
+			end
+	end
+end
+
+get '/test' do
+	bootcampdeck = session['alldecks'].find{|a| a.deckname == "Tech Bootcamp 2014"}
+	randomperson = get_random_photo(bootcampdeck.deckpeople)
+	erb :test, :locals => { :randomperson => randomperson}
+end
+
+get '/:deckname/newperson' do
+	erb :newperson
+end
+
+get '/display' do
+	bootcampdataobj = BootcampData.new
+	session['alldecks'] << bootcampdataobj.bootcamp
+	erb :display, :locals => { :bootcamp => session['alldecks'].find{|a| a.deckname == "Tech Bootcamp 2014"} }
+end
+
 post '/newdeck' do
 	params[:deckname] = Deck.new(params[:deckname], [], params[:deckdescription])
 	newdeck.add_person(params[:name], params[:photourl], params[:description])
 	session['alldecks'] << params[:deckname]
 	erb :results, :locals => {:alldecks => session['alldecks']}
 	redirect '/results'
-end
-
-get '/:deckname/newperson' do
-	erb :newperson
 end
 
 get '/newperson' do
